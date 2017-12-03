2db6fef09f024d3c50f98f6ea7b73018f36e9a76
Jeffrey Chan <jeffreychan637@berkeley.edu>
2017-12-02 22:16:16 -0800

    added main go file

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+    "net/url"
+    "net/http"
+)
+
+var ForecastURL := :"https://api.darksky.net/forecast/";
+
+func getDarkSkyAPIkey() {
+
+}
+
+func buildURL() {
+
+}
+
+func makeForecastRequest() {
+
+}
+
+func processForecastResponse() {
+
+}
+
+func outputForecast()
+
+func main() {
+    fmt.Println("Hello world!")
+
+}
