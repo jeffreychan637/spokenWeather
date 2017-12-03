2c56a01c12ac596f466ab917ae1dbcd0f45796fe
Jeffrey Chan <jeffreychan637@berkeley.edu>
2017-12-03 00:16:42 -0800

    added fetching of weather forecast data from Dark Sky API for SF

--- main.go
+++ main.go
@@ -1,32 +1,91 @@
 package main
 
 import (
+    "bufio"
+    // "encoding/json"
     "fmt"
     "net/url"
     "net/http"
+    "io/ioutil"
+    "os"
+    "strings"
 )
 
-var ForecastURL := :"https://api.darksky.net/forecast/";
+var baseForecastURL string = "https://api.darksky.net/forecast/";
 
-func getDarkSkyAPIkey() {
+func checkError(err error) {
+    if (err != nil) {
+        panic(err)
+    }
+}
+
+func getDarkSkyAPIkey() string {
+    data, err := ioutil.ReadFile("secret")
+    checkError(err)
+
+    // Get first line without newline
+    return strings.Split(string(data), "\n")[0]
+}
+
+func getLocations() map[string]string {
+    file, err := os.Open("locations")
+    checkError(err)
+    defer file.Close()
+
+    locations := make(map[string]string)
+
+    reader := bufio.NewReader(file)
+    // todo: use a while loop and loop till end
+    // of file instead of just getting first value
+    firstLine, err := reader.ReadString('\n')
+    checkError(err)
+
+    var locationLine []string = strings.Split(firstLine, " : ")
+    locations[locationLine[0]] = strings.Split(locationLine[1], "\n")[0] // remove newline
+
+    fmt.Println(locations)
+    return locations
+}
+
+func buildURL(apiKey string, location string) string {
+    queryParams := url.Values{}
+    queryParams.Set("exclude", "flags")
+    qPs := queryParams.Encode()
+    forecastURL := baseForecastURL + apiKey + "/" + location + "?" + qPs
+
+    fmt.Println(forecastURL)
+    return forecastURL
+}
+
+func makeForecastRequest(forecastURL string) []byte {
+    response, err := http.Get(forecastURL)
+    checkError(err)
+    defer response.Body.Close()
+    body, err := ioutil.ReadAll(response.Body)
+    checkError(err)
+
+    fmt.Println(string(body))
+    return body
+}
+
+func formatForecastResponse() {
 
 }
 
-func buildURL() {
+func processForecastData() {
 
 }
 
-func makeForecastRequest() {
+func outputForecast() {
 
 }
-
-func processForecastResponse() {
-
-}
-
-func outputForecast()
 
 func main() {
-    fmt.Println("Hello world!")
-
+    apiKey := getDarkSkyAPIkey()
+    locations := getLocations()
+    location := locations["San Francisco"]
+    // todo: update with concurrency with one
+    // thread per location in locations
+    forecastURL := buildURL(apiKey, location)
+    makeForecastRequest(forecastURL)
 }
